b036c1efa48f8fbcb12eaa02f6f4b416bbdf407f
danieledisco <daniele.disco.1965@gmail.com>
2024-03-08 09:17:04 +0100

    Add logic

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+/**
+ * Logica da implementare:
+ * 0 - Definiamo lunghezza della password
+ * 1 - Creiamo una stringa contenente tutte le lettere maiuscole e minuscole i numeri ed i simboli che vogliamo considerare.
+ * 2 - Effettuiamo un loop sulla lunghezza e generiamo un numero casuale tra 0 e la lunghezza della stringa creata prima
+ * 3 - accodiamo alla password il carattere selezioato
+ * 
+ * NB cercando è emerso che per una migliore generazione di numeri casuali interi invece di rand() è consigliato usare 
+ * random_in()
+ */
+
+$characters = ['abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ@#£$%?!'];
+
+
 
 ?>
 
